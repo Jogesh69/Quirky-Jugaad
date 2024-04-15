2f99fbb047386fa4a084f22c09856a10833f8264
ArnavUpadhyay7 <arnavupadhyay7@gmail.com>
2024-04-15 23:07:28 +0530

    UPDATED

--- cart.js
+++ cart.js
@@ -24,7 +24,7 @@ function dynamicCartSection(ob,itemCounter)
     boxDiv.appendChild(boxImg)
 
     let boxh3 = document.createElement('h3')
-    let h3Text = document.createTextNode(ob.name + ' × ' + itemCounter)
+    let h3Text = document.createTextNode(ob.name + ' x ' + itemCounter)
     // let h3Text = document.createTextNode(ob.name)
     boxh3.appendChild(h3Text)
     boxDiv.appendChild(boxh3)
